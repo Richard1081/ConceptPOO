cf27478445e8a5a83d37e4625b7ed7d4fad094b3
Rich10 <Rich10@DESKTOP-KO18HFC>
2022-10-01 23:23:23 +0200

    finish day class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("Hello, World!");
+try
+{
+	Console.WriteLine(new Date(2022, 10, 10));
+	Console.WriteLine(new Date(1981, 7, 10));
+	Console.WriteLine(new Date(1985, 2, 48));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.ToString());
+}
